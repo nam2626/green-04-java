76205fd8c6c2e035e8cc27951a18ba882ba22e19
NamSangKyu <nam2626@naver.com>
2026-04-22 14:18:08 +0900

    Add E02_VariableType class demonstrating variable types in Java

---
+++ Step02/src/E02_VariableType.java
@@ -0,0 +1,37 @@
+
+public class E02_VariableType {
+
+	public static void main(String[] args) {
+		int i = 2147483647;
+		System.out.println(i);
+		
+		long l = 9223372036854775807L;
+		l = 1L;
+		System.out.println(l);
+		byte b = 127;
+		System.out.println(b);
+		b++;
+		System.out.println(b);
+		
+		float f = 3.14F;
+		double d = 3.14;
+		
+		System.out.println(f);
+		System.out.println(d);
+		
+		char ch1 = 'A';
+		char ch2 = '가';
+		System.out.println(ch1);
+		System.out.println(ch2);
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
