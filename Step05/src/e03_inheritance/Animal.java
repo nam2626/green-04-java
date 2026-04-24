cd0136ce4cb6d2d7f8d2a09492a42cd290baccd2
NamSangKyu <nam2626@naver.com>
2026-04-24 17:21:14 +0900

    docs: Step05 상속(inheritance) 기초 예제 주석 추가

---
+++ Step05/src/e03_inheritance/Animal.java
@@ -0,0 +1,13 @@
+package e03_inheritance;
+
+/**
+ * 상속 관계에서 부모 클래스(Parent Class / Super Class) 역할을 함
+ */
+public class Animal {
+	/**
+	 * 자식 클래스들이 공통적으로 가지는 동작을 정의
+	 */
+	public void eat() {
+		System.out.println("동물이 먹이를 먹습니다.");
+	}
+}
---
+++ Step05/src/e03_inheritance/AnimalMain.java
@@ -0,0 +1,32 @@
+package e03_inheritance;
+
+public class AnimalMain {
+	/**
+	 * 다형성(Polymorphism)을 활용한 메서드
+	 * Animal 타입의 매개변수는 Animal뿐만 아니라 그 자식 객체(Human 등)도 전달받을 수 있음
+	 */
+	public static void eat(Animal a) {
+		// 전달된 실제 객체(인스턴스)의 오버라이딩된 메서드가 호출됨
+		a.eat();
+	}
+	
+	public static void main(String[] args) {
+		Animal animal = new Animal();
+		Human human = new Human();
+		
+		// 1. 일반적인 메서드 호출
+		animal.eat();
+		human.eat();
+		
+		System.out.println("--------------------");
+		
+		// 2. 다형성을 이용한 호출 (Upcasting)
+		// 자식 객체는 부모 타입의 변수에 담길 수 있으며, 메서드 호출 시 자식의 기능이 실행됨
+		eat(animal);
+		eat(human);
+	}
+
+}
+
+
+
