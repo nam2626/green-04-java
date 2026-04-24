52b132fd1a533adf82255488d8854bcde33c6844
NamSangKyu <nam2626@naver.com>
2026-04-24 12:15:21 +0900

    feat: Implement StudentService class with sample StudentVO data initialization

---
+++ StudentProject_ver1/src/service/StudentService.java
@@ -0,0 +1,33 @@
+package service;
+
+import vo.StudentVO;
+
+public class StudentService {
+	//필드
+	//StudentVO를 배열로 선언
+	private StudentVO[] arr;
+	private int idx;
+
+	//기본생성자 - 필드 초기화
+	public StudentService() {
+		arr = new StudentVO[20];
+		
+		//샘플데이터 5건 생성해서 배열에 저장
+		arr[idx++] = new StudentVO("20230001", "홍길동", "컴퓨터공학과", 4.5);
+		arr[idx++] = new StudentVO("20230002", "김철수", "경영학과", 3.8);
+		arr[idx++] = new StudentVO("20230003", "이영희", "심리학과", 3.2);
+		arr[idx++] = new StudentVO("20230004", "박영수", "생활체육학과", 4.0);
+		arr[idx++] = new StudentVO("20230005", "최민수", "전자공학과", 3.5);	
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
