5487b9c967611e5e97692de106c830361a832e43
NamSangKyu <nam2626@naver.com>
2026-04-27 14:18:36 +0900

    feat: IotMain 클래스 추가 및 IoT 앱 테스트 완료

---
+++ Step05/src/e05_iot/IotMain.java
@@ -0,0 +1,13 @@
+package e05_iot;
+
+public class IotMain {
+
+	public static void main(String[] args) {
+		IotApp app = new IotApp();
+		
+		app.allDevicePowerOn();
+		
+		app.allDevicePowerOff();
+	}
+
+}
