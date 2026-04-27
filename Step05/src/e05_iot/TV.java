b82fbddbc4c29047fb54960e501ad2613cf6bc88
NamSangKyu <nam2626@naver.com>
2026-04-27 13:46:39 +0900

    feat: e05_iot 패키지에 Power를 상속받는 TV 클래스 추가

---
+++ Step05/src/e05_iot/TV.java
@@ -0,0 +1,25 @@
+package e05_iot;
+
+public class TV extends Power{
+	private boolean power;
+
+	@Override
+	public void powerOnOff() {
+		power = !power;
+		System.out.println(power ? "TV 전원 On" : "TV 전원 Off");
+	}
+
+	@Override
+	public void powerOn() {
+		power = true;
+		System.out.println("TV 전원 On");
+	}
+
+	@Override
+	public void powerOff() {
+		power = true;
+		System.out.println("TV 전원 Off");
+	}
+	
+	
+}
