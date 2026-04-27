dcf8a2ebf8dda730c60225d7cbdc79d3356e99ed
NamSangKyu <nam2626@naver.com>
2026-04-27 13:43:06 +0900

    feat: e05_iot 패키지 추가 및 Power 클래스 구현

---
+++ Step05/src/e05_iot/Power.java
@@ -0,0 +1,17 @@
+package e05_iot;
+/**
+ * 전원 기본 인터페이스
+ */
+public class Power {
+	public void powerOnOff() {
+		System.out.println("powerOnOff");
+	}
+	
+	public void powerOn() {
+		System.out.println("powerOn");
+	}
+	
+	public void powerOff() {
+		System.out.println("powerOff");
+	}
+}
