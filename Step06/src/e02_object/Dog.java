fab5d0f1d56bad4ed880b924dfcf6feb0dce2400
NamSangKyu <nam2626@naver.com>
2026-04-28 14:02:51 +0900

    feat: Implement Dog class with properties, cloning, and toString method

---
+++ Step06/src/e02_object/Dog.java
@@ -0,0 +1,38 @@
+package e02_object;
+
+public class Dog implements Cloneable{
+	private String name;
+	private int age;
+
+	public Dog(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+}
