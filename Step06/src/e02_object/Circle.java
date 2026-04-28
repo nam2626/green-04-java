c439d5f93e3fe2a22f2c42d49ec582acddbb99bd
NamSangKyu <nam2626@naver.com>
2026-04-28 14:08:11 +0900

    Step06 e02_object 패키지 전체 소스코드 주석 추가

--- Step06/src/e02_object/Circle.java
+++ Step06/src/e02_object/Circle.java
@@ -1,8 +1,12 @@
 package e02_object;
 
-public class Circle implements Cloneable{
-	private Point pos;
-	private int r;
+/**
+ * 원을 표현하는 클래스
+ * 참조 타입 필드(Point)를 가지고 있어 깊은 복사(Deep Copy)가 필요함
+ */
+public class Circle implements Cloneable {
+	private Point pos; // 원의 중심점 (참조 타입)
+	private int r;      // 반지름 (기본 타입)
 
 	public Circle(Point pos, int r) {
 		this.pos = pos;
@@ -30,19 +34,22 @@ public class Circle implements Cloneable{
 		return "Circle [pos=" + pos + ", r=" + r + "]";
 	}
 
+	/**
+	 * 객체 복제를 수행하는 메서드
+	 * 참조형 필드가 있는 경우, 해당 필드도 함께 복제(깊은 복사)해야 함
+	 */
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
-		//객체 복사의 경우 깊은복사와 얕은 복사 체크
-		//참조형의 경우는 반드시 깊은 복사를 수행
-		//깊은 복사는 참조형의 경우 메모리 주소가 복사되니까
-		//새 객체를 만들어서 저장
-//		Circle temp = (Circle) super.clone();
-//		temp.setPos((Point) pos.clone());
-//		return temp;
+		// 1. 얕은 복사(Shallow Copy): super.clone()은 참조 주소만 복사함
+		// Circle temp = (Circle) super.clone();
+		
+		// 2. 깊은 복사(Deep Copy): 참조하고 있는 객체(Point)도 새로 생성하여 저장
+		// temp.setPos((Point) pos.clone());
+		// return temp;
+		
+		// 아래 방식은 새로운 Circle 객체를 생성하면서 내부 Point 객체도 복제하여 전달함
 		return new Circle((Point)pos.clone(), r);
 	}
-
-	
 	
 }
 
--- Step06/src/e02_object/DogMain.java
+++ Step06/src/e02_object/DogMain.java
@@ -1,15 +1,23 @@
 package e02_object;
 
+/**
+ * Dog 객체의 복제 기능을 테스트하는 클래스
+ */
 public class DogMain {
 
 	public static void main(String[] args) throws CloneNotSupportedException {
+		// 1. 원본 객체 생성
 		Dog d1 = new Dog("흰둥이", 5);
+		
+		// 2. 객체 복제
 		Dog d2 = (Dog) d1.clone();
 		
+		// 3. 원본 데이터 변경
 		d1.setName("깜둥이");
 		
-		System.out.println(d1);
-		System.out.println(d2);
+		// 결과 출력 (원본은 "깜둥이", 복제본은 여전히 "흰둥이"여야 함)
+		System.out.println("원본 d1: " + d1);
+		System.out.println("복제본 d2: " + d2);
 	}
 
 }
