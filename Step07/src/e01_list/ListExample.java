c7ae045d00747c1bdcdfc9bbc4faff5ad68addf5
NamSangKyu <nam2626@naver.com>
2026-04-28 17:00:56 +0900

    feat: Add ListExample class demonstrating ArrayList usage and characteristics

---
+++ Step07/src/e01_list/ListExample.java
@@ -0,0 +1,28 @@
+package e01_list;
+
+import java.util.ArrayList;
+
+public class ListExample {
+	/*
+	 * 	List 특징
+	 * 		1. 데이터 중복
+	 * 		2. 리스트에 저장된 데이터 순서가 보장됨
+	 * 
+	 *  ArrayList : 리스트에서 데이터를 배열로 관리
+	 *  LinkedList : 리스트에서 데이터를 노드 단위로 노드끼리 연결해서 관리
+	 *  Vector : ArrayList와 동일	, 동기화가 추가
+	 * 
+	 */
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		
+		
+	}
+
+}
+
+
+
+
+
+
