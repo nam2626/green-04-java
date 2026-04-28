3e6b57078e7b7badb3d404b864c1091aaa658323
NamSangKyu <nam2626@naver.com>
2026-04-28 14:01:24 +0900

    feat: Update Circle class to implement deep cloning for position reference

--- Step06/src/e02_object/Circle.java
+++ Step06/src/e02_object/Circle.java
@@ -32,7 +32,14 @@ public class Circle implements Cloneable{
 
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
-		return super.clone();
+		//객체 복사의 경우 깊은복사와 얕은 복사 체크
+		//참조형의 경우는 반드시 깊은 복사를 수행
+		//깊은 복사는 참조형의 경우 메모리 주소가 복사되니까
+		//새 객체를 만들어서 저장
+//		Circle temp = (Circle) super.clone();
+//		temp.setPos((Point) pos.clone());
+//		return temp;
+		return new Circle((Point)pos.clone(), r);
 	}
 
 	
