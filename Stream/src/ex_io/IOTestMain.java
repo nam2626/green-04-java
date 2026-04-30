f65abfedd38ea161ff663b8aa52bcffb0e1335b2
NamSangKyu <nam2626@naver.com>
2026-04-30 16:55:08 +0900

    feat: add IOTest and IOTestMain classes for input/output operations

---
+++ Stream/src/ex_io/IOTestMain.java
@@ -0,0 +1,12 @@
+package ex_io;
+
+public class IOTestMain {
+
+	public static void main(String[] args) {
+		IOTest io = new IOTest();
+		System.out.println(io.readline());
+		io.println("Test");
+		
+	}
+
+}
