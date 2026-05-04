5aa98e5832ea35421b5b94855d1f26c4feb2f0b6
NamSangKyu <nam2626@naver.com>
2026-05-04 10:45:55 +0900

    feat: add FileCopyExample02 for copying files using a loop and handling exceptions

--- Stream/src/e03_object_io/FileCopyExample02.java
+++ Stream/src/e03_object_io/FileCopyExample02.java
@@ -5,17 +5,19 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class FileCopyExample01 {
+public class FileCopyExample02 {
 
 	public static void main(String[] args) {
+		// 1. 복사할 원본파일(A)을 스트림으로 연결 --> FileInputStream
+		// 2. 복사할 곳 파일(B)을 스트림으로 연결 --> FileOutputStream
+		// 3. A에서 데이터를 읽고, B에다가 출력 --> 3번과정을 반복해서 끝나면 파일 복사가 완료되는 시점
 		try (FileInputStream fis = new FileInputStream("img.jpg");
 				FileOutputStream fos = new FileOutputStream("copy.jpg");) {
 			long startTime = System.currentTimeMillis();
-			byte[] buffer = new byte[1024];
 			while(true) {
-				int n = fis.read(buffer);
+				int n = fis.read();
 				if(n == -1) break;
-				fos.write(buffer,0,n);
+				fos.write(n);
 			}
 			long endTime = System.currentTimeMillis();
 			fos.flush();
